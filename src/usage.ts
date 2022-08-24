d9a96ffd6eebcebb0365bd3e97ac39a8929cc5ce
Tobias <octobi+github@mailbox.org>
2022-08-24 15:27:00 +0200

    Replace urls to target this fork repo octobi

--- src/usage.ts
+++ src/usage.ts
@@ -5,13 +5,13 @@ function usage() {
   console.error(`
 Usage, if you only have curl, unzip and sh:
 
-         curl -fsSL https://raw.githubusercontent.com/hugojosefson/proxmox-root-on-encrypted-zfs/main/src/cli.ts \\
+         curl -fsSL https://raw.githubusercontent.com/octobi/proxmox-root-on-encrypted-zfs/main/src/cli.ts \\
          | sudo sh -s <command...>
 
 
 Usage, if you have deno:
 
-         sudo deno -A --unstable https://raw.githubusercontent.com/hugojosefson/proxmox-root-on-encrypted-zfs/main/src/cli.ts <command...>
+         sudo deno -A --unstable https://raw.githubusercontent.com/octobi/proxmox-root-on-encrypted-zfs/main/src/cli.ts <command...>
 
 
 Usage, if you have deno, and have cloned this git repo:
