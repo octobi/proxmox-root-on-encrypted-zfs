acd723ab9ae05c49584b632057edff3d824e51e0
Vaslim <vaslim@haha-yes.my.id>
2025-10-09 15:54:30 +0200

    Fix pve repos

--- src/commands/chroot-proxmox.ts
+++ src/commands/chroot-proxmox.ts
@@ -10,11 +10,10 @@ echo EDITOR=vim >> /etc/environment
 byobu-enable
 
 apt install -y wget
-echo "deb [arch=amd64] http://download.proxmox.com/debian/pve trixie pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/proxmox-archive-keyring.gpg] http://download.proxmox.com/debian/pve trixie pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 
-cd /etc/apt/trusted.gpg.d/
-wget https://enterprise.proxmox.com/debian/proxmox-release-trixie.gpg -O proxmox-release-trixie.gpg
-echo '136673be77aba35dcce385b28737689ad64fd785a797e57897589aed08db6e45 *proxmox-release-trixie.gpg' | sha256sum --check --strict
+wget https://enterprise.proxmox.com/debian/proxmox-archive-keyring-trixie.gpg -O /usr/share/keyrings/proxmox-archive-keyring.gpg
+echo '136673be77aba35dcce385b28737689ad64fd785a797e57897589aed08db6e45 /usr/share/keyrings/proxmox-archive-keyring.gpg' | sha256sum --check --strict
 
 apt update
 apt full-upgrade -y
